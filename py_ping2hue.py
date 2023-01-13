ac685a86867bbc7c93a2ad9bf3c2368d284a6beb
Oliver Pieper <pieper@viaregio.de>
2023-01-13 23:06:37 +0100

    TOML-config; brightness variation
    Moved from dotenv to toml for configuration.
    Added brightness variation (red = lighter, green = darker).

--- py_ping2hue.py
+++ py_ping2hue.py
@@ -1,37 +1,54 @@
 import os
+from pathlib import Path
 from time import sleep
 
 import requests
-from dotenv import load_dotenv
 from loguru import logger
 from ping3 import ping
 
-load_dotenv()
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
 
-HUE_BRIDGE_ADDRESS = os.getenv("HUE_BRIDGE_ADDRESS")
-HUE_API_KEY = os.getenv("HUE_API_KEY")
+cfg = tomllib.loads(Path("config.toml").read_text(encoding="utf-8"))
+# assign config values to "constants" to improve code legibility
+HUE_BRIDGE_ADDRESS = cfg["bridge"]["host"]
+HUE_API_KEY = cfg["bridge"]["api_key"]
+HUE_LIGHT_IDS = cfg["lights"]["ids"]
+PING_HOST = cfg["ping"]["host"]
+PING_GREEN = cfg["ping_range"]["green"]
+PING_RED = cfg["ping_range"]["red"]
 
-PING_GREEN = 0.05
-PING_RED = 0.40
-
+# hue color values
 HUE_VAL_GREEN = (65535 // 3) + 3000  # green, shifted 3000 towards cyan
 HUE_VAL_RED = 0
 
-HUE_LIGHT_NUMBER = 1
+
+def calculate_quality(response_time: float) -> float:
+    """Calculate ping quality (float from 1.0 to 0.0) from the response_time.
+
+    The quality is 1.0 for pings of PING_GREEN or better,
+    0.0 for pings of PING_RED or worse,
+    and a linear value from 1.0 to 0.0 in between."""
+    if response_time <= PING_GREEN:
+        return 1.0
+    if response_time >= PING_RED:
+        return 0.0
+    return 1 - (response_time - PING_GREEN) / (PING_RED - PING_GREEN)
 
 
-def update_light(hue_val):
-    """Send the new hue value to the selected light.
+def update_lights(hue_val: int, hue_bri: int) -> None:
+    """Send the new hue value to all configured lights.
 
-    On/Off, Brightness and Saturation are not changed, so that the
-    light can be dimmed or turned off independently.
+    The on/off state is not changed, so that the
+    light can be turned on and off independently.
     """
-    url = (
-        f"http://{HUE_BRIDGE_ADDRESS}/api/{HUE_API_KEY}/lights/{HUE_LIGHT_NUMBER}/state"
-    )
-    data = {"hue": hue_val}
 
-    r = requests.put(url, json=data)
+    for l_id in HUE_LIGHT_IDS:
+        url = f"http://{HUE_BRIDGE_ADDRESS}/api/{HUE_API_KEY}/lights/{l_id}/state"
+        data = {"hue": hue_val, "bri": hue_bri, "sat": 255}
+        requests.put(url, json=data)
 
 
 def collect_pings(num_of_pings: int, sleep_time_s: float = 1):
@@ -39,7 +56,7 @@ def collect_pings(num_of_pings: int, sleep_time_s: float = 1):
     total_time = 0
     losses = 0
     for _ in range(num_of_pings):
-        p = ping("google.com", timeout=1, unit="s")
+        p = ping(PING_HOST, timeout=1, unit="s")
         # count packet losses like a "red ping"
         if not p:
             losses += 1
@@ -53,21 +70,26 @@ def collect_pings(num_of_pings: int, sleep_time_s: float = 1):
     return mean_time
 
 
-def ping_to_hue(ping_in_s: float) -> int:
-    """Calculate a hue value from the ping value"""
-    if ping_in_s <= PING_GREEN:
-        return HUE_VAL_GREEN
-    if ping_in_s >= PING_RED:
-        return HUE_VAL_RED
+def calc_hue(quality: float) -> int:
+    """Calculate a hue value from the ping quality value."""
 
-    return int(
-        HUE_VAL_GREEN * (1.0 - (ping_in_s - PING_GREEN) / (PING_RED - PING_GREEN))
-    )
+    return int(HUE_VAL_GREEN * quality + HUE_VAL_RED)
+
+
+def calc_bri(quality: float) -> int:
+    """Calculate hue brightness from the ping value.
+
+    The better the quality, the lower the brightness.
+    """
+    # TODO: make configurable
+    return int(128 * (1 - quality))
 
 
 def main() -> None:
+    """Main loop."""
     while True:
-        update_light(ping_to_hue(collect_pings(5)))
+        q = calculate_quality(collect_pings(5))
+        update_lights(calc_hue(q), calc_bri(q))
 
 
 if __name__ == "__main__":
